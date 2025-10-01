452fef42092838bb5dd5047c5dcffaf8a1797a32
sonali <=>
2025-10-01 15:49:14 +0530

    Named Exports with Aliases

---
+++ named_import_alias.js
@@ -0,0 +1,6 @@
+import {subtract as sub, multiply as mul, division as div} from './math.js';
+let a = 24;
+let b = 6;
+console.log(`Subtraction of ${a} and ${b} is : ${sub(a, b)}`);
+console.log(`Multiplication of ${a} and ${b} is : ${mul(a, b)}`);
+console.log(`Division of ${a} and ${b} is : ${div(a, b)}`);
