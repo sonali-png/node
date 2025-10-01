b1c3f919216f1d74b8fdaeb46f0a88e294d4864d
sonali <=>
2025-10-01 14:12:25 +0530

    Exporting and Importing Module - Named Export

---
+++ math.js
@@ -0,0 +1,13 @@
+export function add(a, b) {
+    return a+b;
+}
+export function subtract(a, b) {
+    return a - b;
+}
+export function multiply(a, b) {
+    return a * b;
+}
+export function division(a, b) {
+    return a / b;
+}
+export const PI = 3.14;
---
+++ operations.mjs
@@ -0,0 +1,6 @@
+import {subtract, multiply, division} from './math.js';
+let a = 24;
+let b = 6;
+console.log(`Subtraction of ${a} and ${b} is : ${subtract(a, b)}`);
+console.log(`Multiplication of ${a} and ${b} is : ${multiply(a, b)}`);
+console.log(`Division of ${a} and ${b} is : ${division(a, b)}`);
